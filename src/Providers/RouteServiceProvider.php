fa4fffe8e6583bcfbe7b2db829dedfd294d63a56
Raffael Kessler <raffael.kessler@prowect.com>
2016-12-26 09:23:13 +0100

    RouteServiceProvider

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -7,15 +7,5 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class RouteServiceProvider extends ServiceProvider
 {
-	protected $namespace = 'App\Http\Controllers';
 
-	public function map()
-	{
-		$routesDirectory = DRIPS_DIRECTORY . DIRECTORY_SEPARATOR . 'routes';
-		foreach(glob($routesDirectory . DIRECTORY_SEPARATOR . '*.php') as $routeFile){
-			Route::group(['namespace' => $this->namespace], function($router) use ($routeFile) {
-				require_once $routeFile;
-			});
-		}
-	}
 }
